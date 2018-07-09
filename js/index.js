e9b0f01aa1fd1e69426b2a3bc23634ca5970803a
sach11 <sachin.chopra@myntra.com>
2018-07-09 20:41:55 +0530

    added React Changes

--- js/index.js
+++ js/index.js
@@ -1,9 +1,43 @@
 console.log("Script Execution Started");
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      products: []
+    }
+  }
+  componentDidMount() {
+    fetch('/products/nike')
+    .then(r => {
+      if (r.ok) {
+        return r.json();
+      }
+      throw new Error('Error in fetching');
+    })
+    .then(resp => this.setState({
+      products: resp.products
+    }))
+    .catch(e => {
+      console.error('Something crashed - ', e);
+    })
+  }
+
   render() {
     return (
-      <div> Hello World </div>
+      this.state.products.map(thisProduct => (
+          <div className='product-block'> 
+            <div> {thisProduct.product} </div>
+            <div>
+              <img 
+                style={{ width: '128px', height: '128px' }}
+                src={thisProduct.search_image}
+              />
+            </div>
+            <button className="buy-button"> Buy Now </button>
+          </div>
+        )
+      )
     );
   }
 }
